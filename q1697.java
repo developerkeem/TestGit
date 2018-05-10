0d4f1e582c64d428370eed111cd891b22da03398
developerkeem <keienar@naver.com>
2018-05-10 17:31:04 +0900

    Question1697

---
+++ q1697.java
@@ -0,0 +1,27 @@
+package On0219;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+/*bfs**/
+public class q1697 {
+	static int N,K;
+	static int time=0;
+	//Queue<LinkedList> q;
+	//q = new Queue<>();
+	//q.
+	
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N=Integer.parseInt(st.nextToken());
+		K=Integer.parseInt(st.nextToken());
+		
+
+	}
+
+}
